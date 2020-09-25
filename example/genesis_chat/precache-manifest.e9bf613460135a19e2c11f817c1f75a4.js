231fbc210d13b036a349ccfbbf3dab1907b6befe
madrogalo <youn4ik@gmail.com>
2020-09-25 19:03:21 +0300

    genesis chat

---
+++ example/genesis_chat/precache-manifest.e9bf613460135a19e2c11f817c1f75a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bcbce5ff07fe05f62a3524b6f66d68b",
+    "url": "/example/genesis_chat/index.html"
+  },
+  {
+    "revision": "26bf49df93fe9d073c81",
+    "url": "/example/genesis_chat/static/css/main.d93176ac.chunk.css"
+  },
+  {
+    "revision": "9dc443e1b9573d31ab68",
+    "url": "/example/genesis_chat/static/js/2.1b60a52e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/example/genesis_chat/static/js/2.1b60a52e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26bf49df93fe9d073c81",
+    "url": "/example/genesis_chat/static/js/main.6366c21d.chunk.js"
+  },
+  {
+    "revision": "6e1a058dfb8b7b453976",
+    "url": "/example/genesis_chat/static/js/runtime-main.0770187a.js"
+  }
+]);
